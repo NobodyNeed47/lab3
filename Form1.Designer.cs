155247bb5e3f0a3806a47e453dc25b5d36737d6a
NobodyNeed47 <118775642+NobodyNeed47@users.noreply.github.com>
2022-11-22 09:38:23 +0800

    Добавлены первоначальный интерфейс

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.z1 = new System.Windows.Forms.TextBox();
+            this.z2 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(57, 39);
+            this.x1.Margin = new System.Windows.Forms.Padding(4);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(45, 26);
+            this.x1.TabIndex = 0;
+            this.x1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(111, 39);
+            this.y1.Margin = new System.Windows.Forms.Padding(4);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(45, 26);
+            this.y1.TabIndex = 1;
+            this.y1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(111, 76);
+            this.y2.Margin = new System.Windows.Forms.Padding(4);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(45, 26);
+            this.y2.TabIndex = 2;
+            this.y2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // z1
+            // 
+            this.z1.Location = new System.Drawing.Point(165, 39);
+            this.z1.Margin = new System.Windows.Forms.Padding(4);
+            this.z1.Name = "z1";
+            this.z1.Size = new System.Drawing.Size(45, 26);
+            this.z1.TabIndex = 3;
+            this.z1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // z2
+            // 
+            this.z2.Location = new System.Drawing.Point(165, 76);
+            this.z2.Margin = new System.Windows.Forms.Padding(4);
+            this.z2.Name = "z2";
+            this.z2.Size = new System.Drawing.Size(45, 26);
+            this.z2.TabIndex = 4;
+            this.z2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(57, 76);
+            this.x2.Margin = new System.Windows.Forms.Padding(4);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(45, 26);
+            this.x2.TabIndex = 5;
+            this.x2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "a";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(57, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "x";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(111, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "y";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(165, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "z";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 32);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Сложение векторов";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 32);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Вычитание векторов";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 32);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Скалярное произведение";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(23, 240);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 32);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Векторное произведение";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(23, 278);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 32);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Длина вектора";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(642, 547);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.z2);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox x1;
+        private TextBox y1;
+        private TextBox y2;
+        private TextBox z1;
+        private TextBox z2;
+        private TextBox x2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
