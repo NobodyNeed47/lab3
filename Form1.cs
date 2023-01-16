b2a67b5837d056005b8ff8c59864fd7f1a8ef677
NobodyNeed47 <118775642+NobodyNeed47@users.noreply.github.com>
2023-01-17 07:00:58 +0800

    Добавлена проверка на вводимость координат

--- Form1.cs
+++ Form1.cs
@@ -4,9 +4,9 @@ namespace lab3
     {
         public struct coord
         {
-            public float x;
-            public float y;
-            public float z;
+            public double x;
+            public double y;
+            public double z;
         }
 
         public Form1()
@@ -14,41 +14,84 @@ namespace lab3
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            coord a = new coord { x = float.Parse(x1.Text), y = float.Parse(y1.Text), z = float.Parse(z1.Text) };
-            coord b = new coord { x = float.Parse(x2.Text), y = float.Parse(y2.Text), z = float.Parse(z2.Text) };
-            MessageBox.Show($"Результатом сложения будет вектор\n c = {Logic.SumVect(a, b)}");
+        private async void button1_Click(object sender, EventArgs e)
+        {        
+            if (Logic.ErrorValue(x1,y1,z1,x2,y2,z2))
+            {
+                coord a = new coord { x = double.Parse(x1.Text), y = double.Parse(y1.Text), z = double.Parse(z1.Text) };
+                coord b = new coord { x = double.Parse(x2.Text), y = double.Parse(y2.Text), z = double.Parse(z2.Text) };
+
+                MessageBox.Show($"Результатом сложения будет вектор\n c = {Logic.SumVect(a, b)}");
+            }
+            else 
+            {
+                MessageBox.Show("Некорректно введены координаты векторов");
+            }        
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            coord a = new coord { x = float.Parse(x1.Text), y = float.Parse(y1.Text), z = float.Parse(z1.Text) };
-            coord b = new coord { x = float.Parse(x2.Text), y = float.Parse(y2.Text), z = float.Parse(z2.Text) };
-            MessageBox.Show($"Результатом разности будет вектор\n c = {Logic.MinusVect(a, b)}");
+            if (Logic.ErrorValue(x1, y1, z1, x2, y2, z2))
+            {
+                coord a = new coord { x = double.Parse(x1.Text), y = double.Parse(y1.Text), z = double.Parse(z1.Text) };
+                coord b = new coord { x = double.Parse(x2.Text), y = double.Parse(y2.Text), z = double.Parse(z2.Text) };
+
+                MessageBox.Show($"Результатом разности будет вектор\n c = {Logic.MinusVect(a, b)}");
+            }
+            else
+            {
+                MessageBox.Show("Некорректно введены координаты векторов");
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            coord a = new coord { x = float.Parse(x1.Text), y = float.Parse(y1.Text), z = float.Parse(z1.Text) };
-            coord b = new coord { x = float.Parse(x2.Text), y = float.Parse(y2.Text), z = float.Parse(z2.Text) };
-            MessageBox.Show($"Результатом скалярного произведения будет = {Logic.ScalProd(a,b):#.##}");
+            if (Logic.ErrorValue(x1, y1, z1, x2, y2, z2))
+            {
+                coord a = new coord { x = double.Parse(x1.Text), y = double.Parse(y1.Text), z = double.Parse(z1.Text) };
+                coord b = new coord { x = double.Parse(x2.Text), y = double.Parse(y2.Text), z = double.Parse(z2.Text) };
+
+
+                MessageBox.Show($"Результатом скалярного произведения будет = {Logic.ScalProd(a, b):#.##}");
+            }
+            else
+            {
+                MessageBox.Show("Некорректно введены координаты векторов");
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            coord a = new coord { x = float.Parse(x1.Text), y = float.Parse(y1.Text), z = float.Parse(z1.Text) };
-            coord b = new coord { x = float.Parse(x2.Text), y = float.Parse(y2.Text), z = float.Parse(z2.Text) };
-            MessageBox.Show($"Результатом вектороного произведения будет вектор\n с = {Logic.VectProd(a,b)}");
+            if (Logic.ErrorValue(x1, y1, z1, x2, y2, z2))
+            {
+                coord a = new coord { x = double.Parse(x1.Text), y = double.Parse(y1.Text), z = double.Parse(z1.Text) };
+                coord b = new coord { x = double.Parse(x2.Text), y = double.Parse(y2.Text), z = double.Parse(z2.Text) };
+
+
+                MessageBox.Show($"Результатом вектороного произведения будет вектор\n с = {Logic.VectProd(a, b)}");
+            }
+            else
+            {
+                MessageBox.Show("Некорректно введены координаты векторов");
+            }
+
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            coord a = new coord { x = float.Parse(x1.Text), y = float.Parse(y1.Text), z = float.Parse(z1.Text) };
-            coord b = new coord { x = float.Parse(x2.Text), y = float.Parse(y2.Text), z = float.Parse(z2.Text) };
+            if (Logic.ErrorValue(x1, y1, z1, x2, y2, z2))
+            {
+                coord a = new coord { x = double.Parse(x1.Text), y = double.Parse(y1.Text), z = double.Parse(z1.Text) };
+                coord b = new coord { x = double.Parse(x2.Text), y = double.Parse(y2.Text), z = double.Parse(z2.Text) };
 
-            MessageBox.Show($"Длина вектора а = {Logic.LengtVect(a):#.##}");
-            MessageBox.Show($"Длина вектора b = {Logic.LengtVect(b):#.##}");
+
+                MessageBox.Show($"Длина вектора а = {Logic.LengtVect(a):#.##}");
+                MessageBox.Show($"Длина вектора b = {Logic.LengtVect(b):#.##}");
+            }
+            else
+            {
+                MessageBox.Show("Некорректно введены координаты векторов");
+            }
         }
     }
 }
--- Logic.cs
+++ Logic.cs
@@ -6,6 +6,7 @@ using System.Numerics;
 using System.Runtime.Intrinsics;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace lab3
 {
@@ -37,10 +38,10 @@ namespace lab3
             return message;
         }
 
-        public static float ScalProd(Form1.coord vect1, Form1.coord vect2)
+        public static double ScalProd(Form1.coord vect1, Form1.coord vect2)
         {
-            float result = vect1.x * vect2.x + vect1.y * vect2.y + vect1.z * vect2.z;
-            return result; 
+            double result = vect1.x * vect2.x + vect1.y * vect2.y + vect1.z * vect2.z;
+            return result;
         }
 
         public static double LengtVect(Form1.coord vect)
@@ -61,8 +62,23 @@ namespace lab3
             result.y = vect1.x * vect2.z - vect1.z * vect2.x;
             result.z = vect1.x * vect2.y - vect1.y * vect2.x;
 
-            message = "{ " + Math.Round(result.x , 2) + " ; " + Math.Round(result.y, 2) * -1 + " ; " + Math.Round(result.z, 2) + " }";
+            message = "{ " + Math.Round(result.x, 2) + " ; " + Math.Round(result.y, 2) * -1 + " ; " + Math.Round(result.z, 2) + " }";
             return message;
+        }
+
+        public static bool ErrorValue(TextBox x1, TextBox y1, TextBox z1, TextBox x2, TextBox y2, TextBox z2)
+        {
+            bool correct = true;
+            if (!(double.TryParse(x1.Text, out _) && double.TryParse(y1.Text, out _) && double.TryParse(z1.Text, out _)))
+            {
+                correct = false;
+            }
+            else if (!(double.TryParse(x2.Text, out _) && double.TryParse(y2.Text, out _) && double.TryParse(z2.Text, out _)))
+            {
+                correct = false;
+            }
+
+            return correct;
         }
 
     }
